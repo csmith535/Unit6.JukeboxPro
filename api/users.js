55251e2f3924ca43d24f41cd90243384faaaa11a
Cameron Smith <cam@Camerons-MacBook-Air.local>
2025-10-22 21:00:36 -0400

    users router started

---
+++ api/users.js
@@ -0,0 +1,21 @@
+import { createUser } from "#db/queries/user";
+import express from "express";
+const router = express.Router();
+export default router;
+
+import requireBody from "#middleware/requireBody";
+import { createToken } from "#utils/jwt";
+
+router.route("/register")
+.post(requireBody(["username", "password"]), async (req, res) => {
+    const { username, password } = req.body;
+    const user = await createUser(username, password);
+
+    const token = createToken({ id: user.id });
+    res.status(201).send(token);
+});
+
+router.route("/login")
+.post(requireBody(["username", "password"]), async (req, res) => {
+
+});
